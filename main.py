61fac6d74fba65d04d86c271dfdd8b61472136a2
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-31 01:13:41 +0000

    proyecto

--- main.py
+++ main.py
@@ -20,6 +20,42 @@ def main():
 
     while True:
         menu()
-        opcion = int(input("ingrese una opcion"))  #  falta tray set  poner solo numeros validos
+        opcion = (input("ingrese una opcion"))  #  falta tray set  poner solo numeros validos
         
-        if == 1
+        if opcion == "1":
+            n = input("ingresa las notas, separadas por espacio (7.0 3.4 4.4)")
+            n = n.split(" ")
+            notas = [float(nota) for nota in n ]
+            print(notas)
+            #break
+        elif opcion == "2":  # promedio, nota mayor, nota menor, aprobados
+            #print("2")
+            #break
+
+
+            try:   
+                promedio = sum(notas) / len(notas)
+                mayor = max(notas)
+                menor = min(notas)
+                aprobado = [ a for a in notas if a >= 4.0]
+                print(aprobado)
+            except ZeroDivisionError:
+                print("no hay notas agregadas")
+
+
+        elif opcion == "3":
+            print(f"""
+                promedio : {promedio}  
+                minimo : {menor}
+                maximo : {mayor}
+                aprobado : {aprobado}
+                  
+            """)
+        
+        else:
+            print("terminado")
+            break
+
+
+if __name__ == "__main__":
+    main()
