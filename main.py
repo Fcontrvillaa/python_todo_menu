e09b408906c1752f6ba325a271913bdc3b144ff6
Fcontrvillaa <fcontreras+1@gmail.com>
2025-05-31 00:05:43 +0000

    primero

---
+++ main.py
@@ -0,0 +1,25 @@
+#sistema de notas
+# 1 ingresar notas
+#calcular estadistica de otas
+#nota mas baja
+#cantidad de aprobados
+
+
+#menu
+def menu():
+    print("""
+Menu de Opciones
+1. ingresar nota
+2. Calcula estadisticas
+3. Resultados
+4. Salir
+""")
+    
+def main():
+    notas = []
+
+    while True:
+        menu()
+        opcion = int(input("ingrese una opcion"))  #  falta tray set  poner solo numeros validos
+        
+        if == 1
